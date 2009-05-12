b5ea0c4b21898e81e2de10d2e3a6d5572974d5a4
Joshua Abbott <joshnabbott@joshua-abbotts-macbook-pro.local>
2009-05-11 23:05:42 -0700

    Commenting out users_path. If you need it, uncomment and implement it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,15 @@ class UsersController < ApplicationController
 
   # GET /users
   # GET /users.xml
-  def index
-    @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
-  end
+  # Uncomment this if you need it.
+  # def index
+  #   @users = User.all
+  # 
+  #   respond_to do |format|
+  #     format.html # index.html.erb
+  #     format.xml  { render :xml => @users }
+  #   end
+  # end
 
   # GET /users/1
   # GET /users/1.xml
